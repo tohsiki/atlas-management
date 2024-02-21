329d5baa98f71e5ed97bd8a47262f0a9eb077bc3
黒沼敏樹 <toshiki.kuronuma9@gmail.com>
2024-02-21 20:23:39 +0900

    初期レコードの登録とデバッグバーの登録を行いました。

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\Models\Users\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use DB;
+
+use App\Models\Users\Subjects;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    public function registerView()
+    {
+        $subjects = Subjects::all();
+        return view('auth.register.register', compact('subjects'));
+    }
+
+    public function registerPost(Request $request)
+    {
+        DB::beginTransaction();
+        // バリデーションの追加
+            // $this->validate($request, [
+            //    'over_name' => 'required|string|max:10',
+            //     'under_name' => 'required|string|max:10',
+            //     'over_name_kana' => 'required|string|max:10|regex:/^[ァ-ヶー]+$/',
+            //     'under_name_kana'  => 'required|string|max:10|regex:/^[ァ-ヶー]+$/',
+            //     'mail_address' => 'required|email|unique:users|max:100',
+            //     'sex' => 'required|in:男性,女性,その他',
+            //     'role' => 'required|in:講師(国語),講師(数学),教師(英語),生徒',
+            //     'password' => 'required|min:8|max:30|'
+            // ]);
+        try{
+            $old_year = $request->old_year;
+            $old_month = $request->old_month;
+            $old_day = $request->old_day;
+            $data = $old_year . '-' . $old_month . '-' . $old_day;
+            $birth_day = date('Y-m-d', strtotime($data));
+            $subjects = $request->subject;
+
+            $user_get = User::create([
+                'over_name' => $request->over_name,
+                'under_name' => $request->under_name,
+                'over_name_kana' => $request->over_name_kana,
+                'under_name_kana' => $request->under_name_kana,
+                'mail_address' => $request->mail_address,
+                'sex' => $request->sex,
+                'birth_day' => $birth_day,
+                'role' => $request->role,
+                'password' => bcrypt($request->password)
+            ]);
+            $user = User::findOrFail($user_get->id);
+            $user->subjects()->attach($subjects);
+            DB::commit();
+            return view('auth.login.login');
+        }catch(\Exception $e){
+            DB::rollback();
+            return redirect()->route('loginView');
+        }
+    }
+}
