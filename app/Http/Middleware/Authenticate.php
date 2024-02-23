6cd4388fdcb335be5382c00615ccc2304a1ce78d
黒沼敏樹 <toshiki.kuronuma9@gmail.com>
2024-02-23 11:40:29 +0900

    「タイムアウトするとエラーが表示される」完了しました。

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-
 class Authenticate extends Middleware
 {
     /**
@@ -12,12 +11,11 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-   protected function redirectTo($request)
-{
-    if (! $request->expectsJson()) {
-        return route('login');
-    } else {
-        return redirect()->route('login');
+
+    protected function redirectTo($request)
+    {
+        if (! $request->expectsJson()) {
+            return route('loginView');
+        }
     }
-}
 }
