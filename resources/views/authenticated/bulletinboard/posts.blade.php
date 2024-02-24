2b83506aedd14033097e05f6d9be8ca44b5917f8
黒沼敏樹 <toshiki.kuronuma9@gmail.com>
2024-02-24 12:20:18 +0900

    投稿にサブカテゴリーが表示されない。

--- resources/views/authenticated/bulletinboard/posts.blade.php
+++ resources/views/authenticated/bulletinboard/posts.blade.php
@@ -4,10 +4,15 @@
 <div class="board_area w-100 border m-auto d-flex">
   <div class="post_view w-75 mt-5">
     <p class="w-75 m-auto">投稿一覧</p>
+    <!-- 投稿を囲っているところ -->
     @foreach($posts as $post)
     <div class="post_area border w-75 m-auto p-3">
       <p><span>{{ $post->user->over_name }}</span><span class="ml-3">{{ $post->user->under_name }}</span>さん</p>
       <p><a href="{{ route('post.detail', ['id' => $post->id]) }}">{{ $post->post_title }}</a></p>
+      <!-- 投稿にサブカテゴリーの表示 -->
+      @foreach($post->subCategories as $sub_category)
+        <span>{{ $sub_category->sub_category }}</span>
+      @endforeach
       <div class="post_bottom_area d-flex">
         <!-- コメントアイコンといいねボタンを囲ってる。 -->
         <div class="d-flex post_status">
@@ -43,6 +48,10 @@
       <ul>
         @foreach($categories as $category)
         <li class="main_categories" category_id="{{ $category->id }}"><span>{{ $category->main_category }}<span></li>
+        <!-- メインカテゴリーに紐づくサブカテゴリーを表示する -->
+         @foreach($category->subCategories as $sub_category)
+           <input type="submit" name="category_word" class="category_btn" value="{{ $sub_category->sub_category }}" form="postSearchRequest">
+         @endforeach
         @endforeach
       </ul>
     </div>
